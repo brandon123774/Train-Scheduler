2f3ead1c207972e64adaa8a0982be3e0a45e1909
Brandon Shankar <bshankar12@comcast.net>
2019-11-09 22:28:18 -0800

    added firebase connection to js

---
+++ assets/app.js
@@ -0,0 +1,13 @@
+ // link to Firebase configuration 
+  var firebaseConfig = {
+    apiKey: "AIzaSyBwmsWLnuJFQguHEsQEIW-BDmstVT--HPA",
+    authDomain: "train-scheduler-b340a.firebaseapp.com",
+    databaseURL: "https://train-scheduler-b340a.firebaseio.com",
+    projectId: "train-scheduler-b340a",
+    storageBucket: "train-scheduler-b340a.appspot.com",
+    messagingSenderId: "708995185463",
+    appId: "1:708995185463:web:42eecefcf1e718cf3fb8b2",
+    measurementId: "G-LDN5911WE1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
